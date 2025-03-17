715fbfdf86654098959ecb60ac5629d4fcd3b5f7
gun kim <gun@whattime.co.kr>
2025-03-18 02:36:29 +0900

    26진법으로 계산

--- sealsedSpell.js
+++ sealsedSpell.js
@@ -1,25 +1,37 @@
 function solution(n, bans) {
-  const alphabet = Array.from({ length: 26 }, (_, i) =>
-    String.fromCharCode(i + 97)
-  );
-  const spellMap = [];
-  function createSpell(max, start, comb) {
-    if (comb.length === max) {
-      const spell = comb.join("");
-      if (bans.includes(spell)) return;
-      return spellMap.push(spell);
-    }
-    for (let i = 0; i < alphabet.length; i++) {
-      comb.push(alphabet[i]);
-      createSpell(max, i, comb);
-      comb.pop();
-    }
-  }
-  let index = 0;
-  while (spellMap.length < n) {
-    index++;
-    createSpell(index, 0, []);
+  let answerN = n;
+
+  const baseBans = bans.map(convertStringToBase26).sort((a, b) => a - b);
+
+  for (const ban of baseBans) {
+    if (ban <= answerN) answerN++;
+    else break;
   }
 
-  return spellMap[n - 1];
+  let length = 1;
+  let count = 26;
+
+  while (answerN > count) {
+    answerN -= count;
+    length++;
+    count *= 26;
+  }
+
+  answerN--;
+  let answer = "";
+
+  for (let i = 0; i < length; i++) {
+    answer = String.fromCharCode(97 + (answerN % 26)) + answer;
+    answerN = Math.floor(answerN / 26);
+  }
+
+  return answer;
+}
+
+function convertStringToBase26(input) {
+  let result = 0;
+  for (const c of input) {
+    result = result * 26 + (c.charCodeAt(0) - 96);
+  }
+  return result;
 }
